b2139980914fcf2563a662314aeec5b399f07860
Haroon Shoukat <haroon5507@gmail.com>
2021-10-29 19:52:05 +0500

    Initial Commit

---
+++ TenTwenty_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TenTwenty_Assignment
+//
+//  Created by Haroon Shoukat on 29/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
